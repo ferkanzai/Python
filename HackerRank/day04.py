e4038bde1cb6bf6ad633df13d6a389dcd5609591
Carmona Ayuela <fca@dca90494c924.ant.amazon.com>
2019-06-03 12:05:35 +0100

    Rewriting HackerRank/day04.py

--- HackerRank/day04.py
+++ HackerRank/day04.py
@@ -1,22 +1,23 @@
 class Person:
-    def __init__(self,initialAge):
-        # Add some more code to run some checks on initialAge
-        if (initialAge < 0):
-            age = 0
-            print('Age is not valid, setting age to 0')
+
+    def __init__(self, initialAge):
+        if initialAge < 0:
+            self.age = 0
+            print("Age is not valid, setting age to 0.")
         else:
-            self.initialAge = age
-    def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
-        if age < 13:
-            print('You are young.')
-        elif (13 <= age < 18):
-            print('You are a teenager.')
-        else:
-            print('You are old.')
+            self.age = initialAge
+
     def yearPasses(self):
-        # Increment the age of the person in here
-        age += 1
+        self.age += 1
+
+    def amIOld(self):
+
+        if self.age < 13:
+            print("You are young.")
+        elif 13 <= self.age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
 
 t = int(input())
 for i in range(0, t):
