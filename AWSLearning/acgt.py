bb661dac7971b8e226b9c3f65ac740a225357970
Carmona Ayuela <fca@dca90494c924.ant.amazon.com>
2019-06-05 16:13:58 +0100

    Modifying acgt.py to create a function of the program

--- AWSLearning/acgt.py
+++ AWSLearning/acgt.py
@@ -1,13 +1,16 @@
-string = 'ACCGGGTTTTAAAA'
-reversed = string[::-1]
-swapped = ''
-for i in range(len(reversed)):
-    if reversed[i] == 'A':
-        swapped = swapped + 'T'
-    elif reversed[i] == 'T':
-        swapped = swapped + 'A'
-    elif reversed[i] == 'C':
-        swapped = swapped + 'G'
-    elif reversed[i] == 'G':
-        swapped = swapped + 'C'
-print(swapped)
+def acgt(word):
+    #string = 'ACCGGGTTTTAAAA'
+    reversed = word[::-1]
+    swapped = ''
+    for i in range(len(reversed)):
+        if reversed[i] == 'A':
+            swapped = swapped + 'T'
+        elif reversed[i] == 'T':
+            swapped = swapped + 'A'
+        elif reversed[i] == 'C':
+            swapped = swapped + 'G'
+        elif reversed[i] == 'G':
+            swapped = swapped + 'C'
+    print(swapped)
+
+acgt(input("Type a string including only A, C, G, T: "))
