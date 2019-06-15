784453cd2de2e7cf50f431e27cda0c3b91adb5c8
ferkanzai <fernando.carayu@gmail.com>
2019-06-15 19:21:46 +0100

    Adding CodeCademy/linkedlist.py

---
+++ CodeCademy/linkedlist.py
@@ -0,0 +1,48 @@
+# We'll be using our Node class
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next_node(self):
+    return self.next_node
+
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+# Our LinkedList class
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+
+  def get_head_node(self):
+    return self.head_node
+
+# Add your insert_beginning and stringify_list methods below:
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+
+  def stringify_list(self):
+    string = ""
+    head_node = self.get_head_node()
+    head_value = head_node.value
+    if head_value != None:
+      string = string + str(head_value) + "\n"
+      next_node = head_node.get_next_node()
+      while next_node is not None:
+        string = string + str(next_node.value) + "\n"
+        next_node = next_node.get_next_node()
+    return string
+
+# Test your code by uncommenting the statements below - did your list print to the terminal?
+ll = LinkedList(5)
+ll.insert_beginning(70)
+ll.insert_beginning(5675)
+ll.insert_beginning(90)
+#print(ll.get_head_node().next_node.next_node.get_value())
+print(ll.stringify_list())
