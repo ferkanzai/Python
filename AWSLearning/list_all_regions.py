fc71c3d80a798ec73122e322ca9843abde4ddd32
Carmona Ayuela <fca@dca90494c924.ant.amazon.com>
2019-06-24 15:13:03 +0100

    using list comprehesion

--- AWSLearning/list_all_regions.py
+++ AWSLearning/list_all_regions.py
@@ -4,10 +4,6 @@ import json
 ec2 = boto3.client('ec2')
 s3 = boto3.client('s3')
 
-regions = ec2.describe_regions()["Regions"]
-list_regions = []
-
-for i in range(len(regions)):
-    list_regions.append(regions[i]["RegionName"])
-
-    
+#regions = ec2.describe_regions()["Regions"]
+list_regions = [ec2.describe_regions()["Regions"][i]["RegionName"] for i in range(len(ec2.describe_regions()["Regions"]))]
+print(list_regions)
