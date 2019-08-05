a22d13d4437b3ddd25eb753183980c0e578fd41b
Fernando Carmona <fernando.carayu@gmail.com>
2019-08-05 21:43:55 +0100

    Update the IP of an S3 bucket policy

---
+++ AWSLearning/update_s3_bp_ip.py
@@ -0,0 +1,38 @@
+import json, re, boto3, urllib.request
+
+s3 = boto3.client('s3')
+bucket_name = 'fcadevplan'
+
+def get_policy():
+    policy = json.dumps(s3.get_bucket_policy(Bucket=bucket_name))
+    return policy
+
+def get_ip():
+    policy = get_policy()
+    ip_policy = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', policy)
+    return ip_policy[0]
+
+def change_ip(ip):
+	policy = get_policy()
+	replaced = json.loads(re.sub(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip, policy))
+	policy_new = replaced["Policy"]
+	new_policy = s3.put_bucket_policy(
+        Bucket=bucket_name,
+        Policy=policy_new
+        )
+	return new_policy
+
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+ip_bucket = get_ip()
+
+print("IP on S3 is:", ip_bucket)
+print("IP on my laptop is:", external_ip)
+
+if ip_bucket == external_ip:
+    print("Your actual IP is already configured in your bucket policy")
+else:
+    print("Changing {} for {}".format(ip_bucket, external_ip))
+    change_ip(external_ip)
+    print("IP on the policy was changed to {}".format(external_ip))
+
+print(get_policy())
