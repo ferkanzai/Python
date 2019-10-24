c17e8f464e88905c9f0fcae1c3f29c852a5b0eb4
Fernando Carmona Ayuela <fernando.carayu@gmail.com>
2019-10-24 17:37:27 +0100

    printing in one column and added tabulate module

--- AWSLearning/list_all_regions.py
+++ AWSLearning/list_all_regions.py
@@ -1,9 +1,11 @@
-import boto3
-import json
+import boto3, json
+#from tabulate import tabulate
 
 ec2 = boto3.client('ec2')
 s3 = boto3.client('s3')
 
 #regions = ec2.describe_regions()["Regions"]
 list_regions = [ec2.describe_regions()["Regions"][i]["RegionName"] for i in range(len(ec2.describe_regions()["Regions"]))]
-print(list_regions)
+#print(tabulate(list_regions, headers=['Region Name'], tablefmt='github'))
+for region in list_regions:
+    print(region)
